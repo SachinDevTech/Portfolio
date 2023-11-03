ce6a23c5f5272c78c39c9794f14adebb6b104ec3
kumarsachin70 <sachin.1107sk@gmail.com>
2023-11-04 00:41:35 +0530

    My portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+const carouselTrack = document.querySelector('.carousel-track');
+
+// Duplicate the carousel content for smooth infinite loop
+carouselTrack.innerHTML += carouselTrack.innerHTML;
+
+// Set the width of carousel track based on number of slides
+const numSlides = document.querySelectorAll('.carousel-slide').length;
+carouselTrack.style.width = `${numSlides * 10}%`; // Each slide takes 20% width
+
+// Calculate the animation duration dynamically based on number of slides
+const animationDuration = numSlides * 2 + 's'; // 2s for each slide transition
+carouselTrack.style.animationDuration = animationDuration;
+
+
+
+
+
+
+// mouse effects
+
+
